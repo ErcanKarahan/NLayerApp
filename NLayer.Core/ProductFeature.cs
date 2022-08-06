3d9e2417ed800ee3413dc8745477a1d015765c5c
Ercan Karahan <ercankarahan@hotmail.de>
2022-08-06 14:42:30 +0300

    Add project files.

---
+++ NLayer.Core/ProductFeature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public class ProductFeature : BaseEntity
+    {
+        public int Id { get; set; }
+
+        public string Color { get; set; }
+
+        public int Hight { get; set; }
+
+        public int widht { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+
+    }
+}
