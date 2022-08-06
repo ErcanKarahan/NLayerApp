18e9963d6bcfe856d635a3634849ad4839ca5956
Ercan Karahan <ercankarahan@hotmail.de>
2022-08-06 15:30:12 +0300

    Added IGenericRepository

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Linq.Expressions;
+
+
+namespace NLayer.Core.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> where(Expression<Func<T, bool>> expression);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync (T entity);        
+        void Update (T entity);
+        void Delete (T entity);
+        Task AddRangeAsync (IEnumerable<T> entity);        
+        void RemoveRange (IEnumerable<T> entity);
+    }
+}
