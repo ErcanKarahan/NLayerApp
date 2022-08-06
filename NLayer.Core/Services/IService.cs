639ede02f06e0647666c6cf42eece9378483c195
Ercan Karahan <ercankarahan@hotmail.de>
2022-08-06 15:41:45 +0300

    Added IService

---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> where(Expression<Func<T, bool>> expression);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entity);
+        Task RemoveRangeAsync(IEnumerable<T> entity);
+    }
+}
