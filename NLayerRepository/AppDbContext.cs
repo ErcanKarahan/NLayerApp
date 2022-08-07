5e2b5439c0a4902ae0aaeb937e416268f0177aba
Ercan Karahan <ercankarahan@hotmail.de>
2022-08-07 21:52:35 +0300

    Added Last Chages

--- NLayerRepository/AppDbContext.cs
+++ NLayerRepository/AppDbContext.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NLayer.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NLayerRepository
 {
