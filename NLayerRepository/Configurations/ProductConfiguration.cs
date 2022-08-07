11caa6961a84a81ca580ee1d1483b4f848f071e0
Ercan Karahan <ercankarahan@hotmail.de>
2022-08-07 19:21:44 +0300

    Added Category and Product Configurations

--- NLayerRepository/Configurations/ProductConfiguration.cs
+++ NLayerRepository/Configurations/ProductConfiguration.cs
@@ -9,16 +9,19 @@ using System.Threading.Tasks;
 
 namespace NLayerRepository.Configurations
 {
-    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
     {
-        public void Configure(EntityTypeBuilder<Category> builder)
+        public void Configure(EntityTypeBuilder<Product> builder)
         {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).UseIdentityColumn();
             builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Stock).IsRequired();
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");
 
-            builder.ToTable("Categories");
 
+            builder.ToTable("Products");
+            builder.HasOne(x => x.Category).WithMany(x => x.Products).HasForeignKey(x => x.CategoryId);
         }
     }
 }
